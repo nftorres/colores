dfc2dfa5cc341304c5874bf1cf276a427255956b
Fabián Torres <nftorresc@gmail.com>
2024-03-09 15:39:09 -0500

    Refactorización

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 const screenColor = document.querySelector(".screen_color");
 const btn = document.querySelector(".btn");
 
-function cambiarColor() {
-    var color = "#";
+const cambiarColor = () => {
+    const generarColorAleatorio = () => {
+        let color = "#";
 
-    for (var i = 0; i < 6; i++) {
-        color += Math.floor(Math.random() * 16).toString(16);
-    }
+        for (var i = 0; i < 6; i++) {
+            color += Math.floor(Math.random() * 16).toString(16);
+        }
+        return color;
+    };
 
+    const color = generarColorAleatorio();
     document.body.style.backgroundColor = color;
     screenColor.textContent = color;
-}
+};
 
 window.addEventListener("load", () => {
     cambiarColor();
